ee196455a9bb342dcae344d2969283ef30cc7ec4
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-09-12 01:18:10 -0600

    Update Ejercicio-01.java

--- Pr2_ejercicio_01/src/Datos_java/ejercicio_01.java
+++ Pr2_ejercicio_01/src/Datos_java/ejercicio_01.java
@@ -1,9 +1,5 @@
-package Datos_java;
 /* ********************* DOCUMENTACION**********************
- * Programa o tarea: Practica_02_Ejercicio_01  
- * 
- * Version: 16 de Febrero del 2023 
- *  
+ 
  * Autor:Sanchez Morales Braulio Eduardo 
  * 
  * Descripcion: Programa que hace el interactue con el usario a traves de ciertas preguntas 
@@ -14,49 +10,40 @@ package Datos_java;
  * Datos de salida: Saludo, resumen de información dada, despedida
  
  */
-        import java.util.Scanner;
 
-		public class ejercicio_01 {
-		static Scanner teclado = new Scanner(System.in);
+package Datos_java;
+import java.util.Scanner;
 
-		public static void main(String[] args) 
-		{
+public class ejercicio_01 {
+static Scanner teclado = new Scanner(System.in);
+public static void main(String[] args) {
 		
-		
-		String nombre,escuela;
-		int edad; 
-		float promedio; 
-		char carrera;
-		System.out.println();
-		
-		System.out.println("Hola buenos dias!");
-		
-		
-		System.out.println("¿Cual es tu nombre?");
-		nombre = teclado.nextLine();
-		
-		System.out.println( " hola! " + nombre   );
-		
-		System.out.println(" ¿Que edad tienes? " );
-		edad = teclado.nextInt();
-		
-		System.out.println(" ¿Con que promedio saliste de la preparatoria? " );
-		promedio = teclado.nextFloat();
-		
-		
-		teclado = new Scanner(System.in);
-		System.out.println(" ¿Como se llamo tu preparatoria? ");
-		escuela = teclado.nextLine();
-		
-		System.out.println("Escribe la primera letra de tu carrera: B (Ing. Biomedica), E (Ing Electronica), C (Lic. en Computacion)");
-		carrera = teclado.next().charAt(0);
-		
-		System.out.println( nombre + " tienes " +  edad + " años"  + " estuviste en " + escuela + " y sacaste " + promedio + " de calificacion " + "la inicial de tu carrera es:" + carrera ); 
-		System.out.println(" Hasta luego! " + nombre );
-		
-		
-		// TODO Auto-generated method stub
+String nombre,escuela;
+int edad; 
+float promedio; 
+char carrera;		
+System.out.println(" Hola! ");		
+System.out.println("¿Cual es tu nombre?");
+nombre = teclado.nextLine();		
+System.out.println( " Que tal " + nombre   );		
 
-	}
+System.out.println(" ¿Que edad tienes? " );
+edad = teclado.nextInt();		
 
+System.out.println(" ¿Con que promedio saliste de la preparatoria? " );
+promedio = teclado.nextFloat();		
+
+System.out.println(" ¿Como se llamo tu preparatoria? ");
+escuela = teclado.nextLine();		
+
+System.out.println("Escribe la primera letra de tu carrera: B (Ing. Biomedica), E (Ing Electronica), C (Lic. en Computacion)");
+carrera = teclado.next().charAt(0);		
+
+System.out.println( nombre + " tienes " +  edad + " años"  + " estuviste en " + escuela + " y sacaste " + promedio + " de calificacion " + "la inicial de tu carrera es:" + carrera ); 
+System.out.println(" Hasta luego! " + nombre );
+
+ }
 }
+
+
+
