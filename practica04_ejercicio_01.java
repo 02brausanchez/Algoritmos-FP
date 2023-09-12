abcf0e3d13ed9fbdd95969c6e38d3f5d6a2168af
02brausanchez <120343735+02brausanchez@users.noreply.github.com>
2023-09-12 01:24:00 -0600

    Update practica04_ejercicio_01.java

--- practica04_ejercicio_01.java
+++ practica04_ejercicio_01.java
@@ -1,51 +1,46 @@
-/*============Documentacion===========
- * Autor: Braulio Eduardo Sanchez Morales 
+/*
+ ============Documentacion===========
+ * Autor: Sanchez Morales Braulio Eduardo 
  * Descripcion: Programa que relata un breve poema
  * Datos de entrada: Ninguno 
  * Datos de salida:Ninguno
  * 
  */
 
-
-
-
-
-
-
 package ejercicios;
  
-public class practica04_ejercicio_01 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" Este es un poema: ");
-		   proc_C();
-		   proc_B();
-		   proc_A();
-		   proc_D();
-		   proc_E(); 
-		   proc_F();
+public class Practica04_Ejercicio_01 {
+public static void proc_A() {
+System.out.println(" Un viento envidioso ");
+}
+public static void proc_C() {
+System.out.println( " Mi arbol tenia ");		
+}
+public static void proc_B() {
+System.out.println(" sus ramas de oro ");
+}
+public static void proc_D() {
+System.out.println(" robo mi tesoro. ");
+System.out.println(" Hoy no tiene ramas. ");
+}
+public static void proc_E() {
+System.out.println(" hoy no tiene sueños ");
+System.out.println(" mi arbol callado ");		
+}
+public static void proc_F() {
+System.out.println(" mi arbol pequeño ");
+}
 
-	}
-	public static void proc_A() {
-		System.out.println(" Un viento envidioso ");
-	}
-	public static void proc_C() {
-		System.out.println( " Mi arbol tenia ");		
-	}
-	public static void proc_B() {
-		System.out.println(" sus ramas de oro ");
-	}
-	public static void proc_D() {
-		System.out.println(" robo mi tesoro. ");
-		System.out.println(" Hoy no tiene ramas. ");
-	}
-	public static void proc_E() {
-		System.out.println(" hoy no tiene sueños ");
-		System.out.println(" mi arbol callado ");		
-	}
-	public static void proc_F() {
-		System.out.println(" mi arbol pequeño ");
-	}
+public static void main(String[] args) {
+System.out.println(" Este es un poema: ");
+proc_C();
+proc_B();
+proc_A();
+proc_D();
+proc_E(); 
+proc_F();
+
+  }
 }
 
 
